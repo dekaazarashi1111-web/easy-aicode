0f1fb6cf057b1a8f5737afa2de821e84adc30624
dekaazarashi1111-web <dekaazarashi1111@gmail.com>
2026-04-02 03:06:53 +0900

    問い合わせ先メールアドレスを設定

--- public/assets/site-config.js
+++ public/assets/site-config.js
@@ -1,6 +1,6 @@
 window.SITE_CONFIG = {
   BRAND_NAME: "ケモホモ作品ファインダー",
-  CONTACT_EMAIL: "",
-  APPLY_EMAIL: "",
+  CONTACT_EMAIL: "dekaazarashi1111@gmail.com",
+  APPLY_EMAIL: "dekaazarashi1111@gmail.com",
   X_PROFILE_URL: ""
 };
